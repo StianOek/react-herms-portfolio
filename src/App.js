67bad497c9b628c695eb0e80f82c2a5bf0c7785e
Stian <stian.oek@gmail.com>
2021-01-01 21:54:43 +0100

    5. Beginning of Styling (styled-components)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react'
 import AboutMe from './pages/AboutMe'
 
 
+import GlobalStyle from './components/GlobalStyle'
+
+
+
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutMe />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,31 +1,74 @@
 import React from 'react'
-import myself from '../img/home1.png';
+import myself from '../img/IMG_4643.JPG';
+
+import styled from 'styled-components';
+
+
 
 const AboutSection = () => {
     return(
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>Name is Stian Herms</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>Front-End developer</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>With <span>UI/UX</span> in mind</h2>
-                    </div>
+                    </Hide>
                     <p>Contact me for any web related or design ideas that you have! I am driven to fetch your ideas out to the world, weather its dekstop or mobile.</p>
                     <button>Contact me</button>
                 </div>
-            </div>
+            </Description>
 
-            <div className="image">
+            <Image>
                 <img src={myself} alt="a pic of myself in about section" />
-            </div>
+            </Image>
         
-        </div>
+        </About>
     )
-}
+};
+
+//Styling
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5rem 10rem;
+    color: white;
+
+`;
+
+const Description  = styled.div`
+    flex: 1;
+    padding-right: 10rem;
+
+    h2 {
+        font-weight: lighter;
+    }
+
+
+`
+
+const Image = styled.div`
+    flex: 1;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+
+`
+
+const Hide = styled.div`
+overflow: hidden;
+`
+
 
 export default AboutSection;

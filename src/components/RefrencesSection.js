975ebecb9a0a30ed7854a948a98ba98462031756
StianOek <66445281+StianOek@users.noreply.github.com>
2021-08-04 09:15:59 +0200

    new design

--- src/components/RefrencesSection.js
+++ src/components/RefrencesSection.js
@@ -46,6 +46,7 @@ const RefrencesSection = () => {
                     <p className="ref_p">"Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>  
                     </div>
                 </Toggle>
+              
             </AnimateSharedLayout>
         </Ref>
     )
